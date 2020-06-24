45928d37401ee92303b019500edfdd1bebfe760d
dt <dt52514@gmail.com>
2020-06-24 13:55:22 +0000

    Paper 1 gitbook compile

---
+++ misc/drive_uploads.R
@@ -0,0 +1,5 @@
+googledrive::drive_auth()
+
+googledrive::drive_upload("../diss/_book/dissertation.pdf", 
+                          path = googledrive::as_dribble("Synced/diss_2020"), 
+                          overwrite = T)
